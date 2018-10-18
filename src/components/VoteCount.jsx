00711adcb922cb24d929d000bf1e7e771b1f641e
Rachael Hall <rachael.hall.889@gmail.com>
2018-10-18 16:24:06 +0100

    arrow buttons are centres, I don't know how or why but yay?

--- src/components/VoteCount.jsx
+++ src/components/VoteCount.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { patchVote } from "./api/patch";
 import "../VoteCount.css";
+// import downArrow from "../assets/baseline-arrow_downward-24px.svg"
+import downArrow from "../assets/baseline_arrow_upward_black_18dp.png";
+import upArrow from "../assets/baseline-arrow_upward-24px.svg";
 
 class VoteCount extends Component {
   state = {
@@ -12,13 +15,20 @@ class VoteCount extends Component {
     const { voteMod } = this.state;
     return (
       <span className="vote-container">
-        <button className="vote-up" onClick={() => this.changeVote("up")} disabled={voteMod === 1}>
-        <i className="material-icons">arrow_upwards</i>
+        <button
+          className="vote-up"
+          onClick={() => this.changeVote("up")}
+          disabled={voteMod === 1}
+        >
+          <i class="material-icons">arrow_upward</i>
         </button>
         <div className="vote-score">{this.props.parent.votes + voteMod}</div>
-        <button className="vote-down"
-          onClick={() => this.changeVote("down")} disabled={voteMod === -1}>
-        <i className="material-icons">arrow_downwards</i>
+        <button
+          className="vote-down"
+          onClick={() => this.changeVote("down")}
+          disabled={voteMod === -1}
+        >
+          <i class="material-icons">arrow_downward</i>
         </button>
       </span>
     );
