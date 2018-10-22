7b8bbe955e25e10ef4a7ac3c7fb34aad78f5fbfe
Rachael Hall <rachael.hall.889@gmail.com>
2018-10-22 10:46:28 +0100

    default user image used in cases of broken links

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import { Link } from "@reach/router";
 import { getOneById } from "./api/get.js";
 import PropTypes from "prop-types";
+import defaultImg from "../assets/default-user.png";
 
 class UserProfile extends Component {
   state = {
@@ -15,7 +16,11 @@ class UserProfile extends Component {
         <h2>User Profile</h2>
         <div>{user.username}</div>
         <div>{user.name}</div>
-        <img src={user.avatar_url} alt={`${user.username}'s avatar`} />
+        <img
+          onError={error => error.target.src = defaultImg}
+          src={user.avatar_url}
+          alt={`${user.username}'s avatar`}
+        />
       </div>
     );
   }
@@ -26,9 +31,14 @@ class UserProfile extends Component {
         this.setState({ user });
       })
       .catch(err => {
-        this.props.navigate('/error', {replace:true, state: {err: err.message}});
+        this.props.navigate("/error", {
+          replace: true,
+          state: { err: err.message }
+        });
       });
   }
+
+  getImage;
 }
 
 UserProfile.propTypes = {
